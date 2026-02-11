18ebf71a3c5971b702d6f565784848fc6502f23c
sameera gundlur shaik <gundlurshaiksameera@gmail.com>
2026-02-11 15:22:35 +0530

    Cleaned and reorganized project structure professionally

---
+++ sql/create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales_data (
+    order_id INTEGER,
+    order_date DATE,
+    customer_name TEXT,
+    product_name TEXT,
+    category TEXT,
+    region TEXT,
+    sales_amount INTEGER,
+    profit INTEGER
+);
